9012ec87dae3634fc30ff917fcd8939acdbcb772
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-04-21 18:54:07 +0200

    Styling_part2_FinishingNavBar

---
+++ code.js
@@ -0,0 +1,22 @@
+//navbar animation//
+
+//varibles//
+
+let links = document.getElementsByTagName("li");
+let circle = document.getElementById("circle");
+
+//functions//
+
+for(link of links){
+  link.onclick = function(){
+
+    for(link of links){
+      link.style.opacity = "1"
+    }
+
+    circle.style.left = (80 * this.value) + "px";
+    circle.innerHTML = this.innerHTML;
+    this.style.opacity = "0"
+  }
+}
+
