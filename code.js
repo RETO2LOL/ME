a0ad2a20b97494fe661516c34a5c590aba8692fd
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-04-23 03:02:02 +0200

    Styling_part3_Dark&Light_part1

--- code.js
+++ code.js
@@ -19,4 +19,38 @@ for(link of links){
     this.style.opacity = "0"
   }
 }
+//navbar animation//
+
+//Dark&Light//
+
+let dark = document.getElementById("NightAndLight")
+let islight = localStorage.getItem("theme") === "dark";
+
+if (islight) {
+  document.documentElement.style.setProperty('--primary-color', ' #424242');
+  document.documentElement.style.setProperty('--square-color', 'rgba(255,255,255,0.5)');
+
+  dark.innerHTML = `<i class="fa-regular fa-lightbulb"></i>`;
+}
+
+
+dark.addEventListener("click", () => {
+  if (!islight) {
+    document.documentElement.style.setProperty('--square-color', 'rgba(255,255,255,0.5)');
+    document.documentElement.style.setProperty('--primary-color', ' #424242');
+    document.documentElement.style.setProperty('--navbar-color', ' #424242');
+    dark.innerHTML = `<i class="fa-regular fa-lightbulb"></i>`;
+     localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.style.setProperty('--primary-color', 'rgba(255,255,255,0.5)');
+    document.documentElement.style.setProperty('--square-color', '#424242');
+    document.documentElement.style.setProperty('--navbar-color', 'rgba(255,255,255,0.5)');
+    dark.innerHTML = `<i class="fa-solid fa-lightbulb"></i>`;
+    localStorage.setItem("theme", "light");
+  }
+  islight = !islight;
+});
+
+
+
 
