97d44533b3cb9089fa4737cdb5a482e126f64e59
RETO2LOL <ammaryasseryasser49@gmail.com>
2026-05-04 11:46:15 +0300

    Styling_part7_AddingCards

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //navbar animation//
 
-//varibles//
+//variables//
 
 let links = document.getElementsByTagName("li");
 let circle = document.getElementById("circle");
@@ -8,10 +8,10 @@ let circle = document.getElementById("circle");
 //functions//
 
 for(link of links){
-  link.onclick = function(){
+  link.onclick = function nav (){
 
-    for(link of links){
-      link.style.opacity = "1"
+    for(li of links){
+      li.style.opacity = "1"
     }
 
     circle.style.left = (80 * this.value) + "px";
@@ -41,7 +41,27 @@ btn.onclick =()=>{
 }
 //Dark&Light//
 
+//card edit//
+const cards = document.querySelectorAll('.card');
+const container = document.querySelector('.cards');
 
+container.addEventListener('mousemove', (e) => {
+  let rect = container.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  
+  let rotateX = -(y - rect.height / 2) / 10;
+  let rotateY = (x - rect.width / 2) / 10;
+  
+  cards.forEach(card => {
+    card.style.setProperty('--rotateX', `${rotateX}deg`);
+    card.style.setProperty('--rotateY', `${rotateY}deg`);
+  });
+});
 
-
-
+container.addEventListener('mouseleave', () => {
+  cards.forEach(card => {
+    card.style.setProperty('--rotateX', `0deg`);
+    card.style.setProperty('--rotateY', `0deg`);
+  });
+});
